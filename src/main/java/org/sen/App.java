a7ee54bf507fefd04e1248fb7b9b34554bfc12b2
InaelGoncalves <inael.wglaia@senacsp.edu.br>
2024-11-13 20:41:35 -0300

    first commit

--- src/main/java/org/sen/App.java
+++ src/main/java/org/sen/App.java
@@ -7,6 +7,7 @@ import org.eclipse.jetty.servlet.ServletHolder;
 /**
  * This class demonstrates how to set up a Jetty server.
  */
+/*Inael*/
 public final class App {
     /**
      * The port number for the Jetty server. aaaaa
